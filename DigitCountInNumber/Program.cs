5e3dbc456a1b7a10d30fb2b5ac73fd8de72406df
katzhukova <katzhukova@gmail.com>
2022-12-25 16:05:33 +0300

    Добавьте файлы проекта.

---
+++ DigitCountInNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitCountInNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите цифру");
+            int digit = int.Parse(Console.ReadLine());
+
+            int digitCount = (int)Math.Log10(number) + 1;    //формула для подсчета количества цифр в числе
+
+            int digitNumber = 0; //цифра числа
+            int count = 0; //счетчик цифр
+            int tempNumber = number;  //временная переменная для хранения заданного числа
+
+            for (int i = 0; i < digitCount; i++)
+            {
+                digitNumber = tempNumber % 10;
+                tempNumber = tempNumber / 10;
+                if (digitNumber == digit)  //определяем сколько раз встречается заданная цифра в заданном числе
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Цифра {digit} в числе {number} встречается - {count} р.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ FirstDigitOfNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDigitOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+
+            int firstDigit = 0;
+
+            for (int i = 0; i < 1; i++)
+            {
+                firstDigit = number % 10;
+            }
+
+            Console.WriteLine($"Первая цифра числа = {firstDigit}");
+            Console.ReadKey();
+        }
+    }
+}
